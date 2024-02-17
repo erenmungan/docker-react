34897b49e184e02d14eb19783e11d45de313c363
Eren Mungan <munganeren@hotmail.com>
2024-02-17 22:27:32 +0300

    add: new text added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Eren Mungan      </p>
+          I love you Gizem      </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Eren Mungan
         </a>
       </header>
     </div>
